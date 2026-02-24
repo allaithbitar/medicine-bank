894a5b272473f82e8114f06418cc215caff2f96b
allaithbitar <allaithbitar.code0@gmail.com>
2026-02-25 02:33:28 +0300

    fixed position for loading overlay

--- src/core/components/common/loading-overlay/loading-overlay.tsx
+++ src/core/components/common/loading-overlay/loading-overlay.tsx
@@ -1,25 +1,19 @@
-import type { Theme } from "@emotion/react";
-import { Backdrop, CircularProgress, type SxProps } from "@mui/material";
+import type { Theme } from '@emotion/react';
+import { Backdrop, CircularProgress, type SxProps } from '@mui/material';
 
-const LoadingOverlay = ({
-  sx,
-  spinnerSize,
-}: {
-  sx?: SxProps<Theme>;
-  spinnerSize?: number;
-}) => (
+const LoadingOverlay = ({ sx, spinnerSize }: { sx?: SxProps<Theme>; spinnerSize?: number }) => (
   <Backdrop
     open
     sx={{
-      position: "absolute",
+      position: 'fixed',
       top: 0,
       left: 0,
-      width: "100%",
-      height: "100%",
-      background: "rgba(255,255,255,0.3)",
-      backdropFilter: "blur(2px)",
-      display: "grid",
-      placeItems: "center",
+      width: '100%',
+      height: '100%',
+      background: 'rgba(255,255,255,0.3)',
+      backdropFilter: 'blur(2px)',
+      display: 'grid',
+      placeItems: 'center',
       zIndex: 999,
       ...sx,
     }}
